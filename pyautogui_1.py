e77999b4dfcda4d319a8918b29615de6918284c2
709433 <41780032+709433@users.noreply.github.com>
2020-08-13 17:26:29 +0800

    Add files via upload

---
+++ pyautogui_1.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+pyautogui.moveTo(2886,429.2)
+#pyautogui.click(clicks=2,interval=2)
+
+pyautogui.doubleClick()
+
+time.sleep(1)
+#pyautogui.typewrite('hello, sein!!!')
+#pyautogui.typewrite(['a','s','k','enter'])
+pyautogui.typewrite(['abc','enter'])
